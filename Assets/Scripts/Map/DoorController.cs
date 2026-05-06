92f04d3d8c468633fab62a1bdad38f2e48a4e3d1
n0wst4ndup <n0wst4ndup@gmail.com>
2026-05-07 03:11:30 +0900

    Feat: 방 미클리어시 가벽 생성
    - WallCollider 및 MeshRender
    - WIP:
      - 다음 방따라 컬러 변경을 못했음
      - Idle2상태일때 움직이면 무브 애니메이션이 재생 안됌

--- Assets/Scripts/Map/DoorController.cs
+++ Assets/Scripts/Map/DoorController.cs
@@ -7,7 +7,7 @@ public class DoorController : MonoBehaviour
 
     [SerializeField] private RoomController _room;
     // 잠금 시 플레이어를 막는 벽 콜라이더 (non-trigger)
-    [SerializeField] private Collider _wallCollider;
+    [SerializeField] private GameObject _wallCollider;
     // 개방 시 플레이어 통과를 감지하는 트리거 콜라이더 (trigger)
     [SerializeField] private Collider _triggerCollider;
 
@@ -26,7 +26,7 @@ public class DoorController : MonoBehaviour
 
     public void SetLocked(bool locked)
     {
-        if (_wallCollider != null) _wallCollider.enabled = locked;
+        if (_wallCollider != null) _wallCollider.SetActive(locked);
         if (_triggerCollider != null) _triggerCollider.enabled = !locked;
     }
 
@@ -36,4 +36,16 @@ public class DoorController : MonoBehaviour
 
         _floorManager?.OnPlayerEnterDoor(_room, Direction);
     }
+
+    // private RoomController GetNextRoom()
+    // {
+    //     return Direction switch
+    //     {
+    //         DoorFlags.North => _floorManager.GetRoom(_room.GridPosition + Vector2Int.up),
+    //         DoorFlags.South => _floorManager.GetRoom(_room.GridPosition + Vector2Int.down),
+    //         DoorFlags.East => _floorManager.GetRoom(_room.GridPosition + Vector2Int.right),
+    //         DoorFlags.West => _floorManager.GetRoom(_room.GridPosition + Vector2Int.left),
+    //         _ => null,
+    //     };
+    // }
 }
