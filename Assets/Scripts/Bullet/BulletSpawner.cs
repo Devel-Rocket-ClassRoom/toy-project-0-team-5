c95a6044f7ef2ce4c19ede813c590784244982ba
JihooKim <82192985+gamjaHoo@users.noreply.github.com>
2026-05-07 21:43:12 +0900

    Feat: 플레이어 발사 사운드 및 bullet 피격 사운드 추가

--- Assets/Scripts/Bullet/BulletSpawner.cs
+++ Assets/Scripts/Bullet/BulletSpawner.cs
@@ -5,10 +5,21 @@ public class BulletSpawner : MonoBehaviour
     [SerializeField] private DefaultBullet _bulletPrefab;
     [SerializeField] private Transform _spawnPoint;
     [SerializeField] private LayerMask _targetLayer;
+    [SerializeField] private AudioClip _fireSound;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     public void SpawnBullet(Vector3 direction, BulletConfig config)
     {
         DefaultBullet bullet = Instantiate(_bulletPrefab, _spawnPoint.position, _spawnPoint.rotation);
         bullet.Init(_targetLayer, direction, config);
+
+        if (_fireSound != null && _audioSource != null)
+            _audioSource.PlayOneShot(_fireSound);
     }
 }
